88b8bb9fb29ac6a0738c95220f1124a05ea3465a
Satori <comfycallum@gmail.com>
2018-07-26 23:13:27 +0100

    Remove FrameLessWindow for now
    May add back once UI is more complete

--- src/xenia/app/main.cc
+++ src/xenia/app/main.cc
@@ -7,13 +7,7 @@
 #endif
 
 int main(int argc, char *argv[]) {
-  // This has the app draw at HiDPI scaling on HiDPI displays, usually two
-  // pixels for every one logical pixel
   QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
-  // This has QPixmap images use the @2x images when available
-  // See this bug for more details on how to get this right:
-  // https://bugreports.qt.io/browse/QTBUG-44486#comment-327410
   QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 
   QApplication app(argc, argv);
@@ -29,27 +23,7 @@ int main(int argc, char *argv[]) {
   windowWidth = 1280;
   windowHeight = 720;
 
-#ifdef _WIN32
-
-  // On Windows, the widget needs to be encapsulated in a native window for
-  // frameless rendering In this case, QWinWidget #includes "Widget.h", creates
-  // it, and adds it to a layout
-  WinNativeWidget w;
-#else
-
-  // On OS X / Linux, the widget can handle everything itself so just create
-  // Widget as normal
   MainWidget w;
-
-#endif
-
-#ifdef __APPLE__
-
-  // Then, hide the OS X title bar
-  // TODO:
-  // OSXHideTitleBar::HideTitleBar(w.winId());
-
-#endif
 
   w.setGeometry(windowXPos, windowYPos, windowWidth, windowHeight);
   w.show();
